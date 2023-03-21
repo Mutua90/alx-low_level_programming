76ca4609af4c3cda26a70bce67dc99433ee28d09
Mutua90 <katiti254@gmail.com>
2023-03-21 04:39:59 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    -putchar.c('-');
+    -putchar.c('p');
+    -putchar.c('u');
+    -putchar.c('t');
+    -putchar.c('c');
+    -putchar.c('h');
+    -putchar.c('a');
+    -putchar.c('r');
+    -putchar.c('\n');
+    return (0);
+}
