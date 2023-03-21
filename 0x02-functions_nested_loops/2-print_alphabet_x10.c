6187967ae55566573f23cc4395f4ac4310615a5c
Mutua90 <katiti254@gmail.com>
2023-03-21 08:53:49 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - make alphabet_x10
+ * Return: void.
+ */
+
+ void print_alphabet_x10(void)
+
+{
+
+char b;
+
+int i = 0;
+
+while (i <= 9)
+
+{ 
+
+for (b = 'a'; b <= 'z'; b++);
+}
+
+{
+
+_putchar(b);
+
+_putchar('\n');
+i++;
+}
+
+}
