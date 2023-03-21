6d6bb8ad668cb8e10f89961d5a044979fda35477
Mutua90 <katiti254@gmail.com>
2023-03-21 05:06:32 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,14 +7,14 @@
  */
 int main(void)
 {
-    -putchar.c('-');
-    -putchar.c('p');
-    -putchar.c('u');
-    -putchar.c('t');
-    -putchar.c('c');
-    -putchar.c('h');
-    -putchar.c('a');
-    -putchar.c('r');
-    -putchar.c('\n');
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
     return (0);
 }
