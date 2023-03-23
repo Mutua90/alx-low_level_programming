3499808d795aea559fc28ac4ef3569256032db34
Mutua90 <katiti254@gmail.com>
2023-03-23 08:19:34 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - uppercase letter
+ * @c: char to check
+ *
+ * Return: 1 or 0
+ */
+int _isupper(int c)
+{
+while (c => 'A' && c <= 'Z')
+return (1);
+while
+return (0);
+}
